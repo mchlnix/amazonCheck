0a96a59da1e9c5699cc071667253227b4a8d35ec
Michael Nix <mchl.nix@googlemail.com>
2013-01-31 22:06:29 +0100

    adds argv routine

--- amazonCheck.py
+++ amazonCheck.py
@@ -4,9 +4,12 @@
 from amazonCheckLib import get_info_for, get_time
 from os.path import expanduser, exists
 from json import dumps, loads
+from sys import argv
 from os import name
 
+
 CONFIG_FILE = expanduser( '~/.amazonCheck.config' )
+DATA_FILE = expanduser( '~/.amazonCheck.data' )
 LOGFILE = expanduser( '~/.amazonCheck.log' )
 
 
@@ -18,22 +21,10 @@ VERBOSE = False
 CONFIG_VARS = 3
 
 
-def reset_config_file():
-
-    new_config_file = open( CONFIG_FILE, 'w' )
-
-    options = [ SILENT, UPDATES_ONLY, VERBOSE ]
-
-    new_config_file.write( dumps( options ) )
-
-    new_config_file.close()
-
-    logfile.write( get_time() + ' Reset Config File at ' + CONFIG_FILE + '\n' )
-
-
 def read_config_file():
 
     if not exists( CONFIG_FILE ):
+
         reset_config_file()
 
         return [ SILENT, UPDATES_ONLY, VERBOSE ]
@@ -53,9 +44,23 @@ def read_config_file():
         return options
 
 
+def reset_config_file():
+
+    new_config_file = open( CONFIG_FILE, 'w' )
+
+    options = [ SILENT, UPDATES_ONLY, VERBOSE ]
+
+    new_config_file.write( dumps( options ) )
+
+    new_config_file.close()
+
+    logfile.write( get_time() + ' Reset Config File at ' + CONFIG_FILE + '\n' )
+
+
 def write_config_file( options ):
 
     if type( options[ 0 ] ) != type( True ) or type( options[ 1 ] ) != type( True ) or type( options[ 2 ] ) != type( True ):
+
         config_file = open( CONFIG_FILE, 'w' )
 
         config_file.write( dumps( options ) )
@@ -64,12 +69,39 @@ def write_config_file( options ):
 
     logfile.write( get_time() + ' Wrote to Config File at ' + CONFIG_FILE + '\n' )
 
+
 if __name__ == '__main__':
+
     logfile = open( LOGFILE, 'a' )
+    logfile.write( get_time() + ' -------------------------------' + '\n' )
+    logfile.write( get_time() + ' Started Program' + '\n' )
 
     [ SILENT, UPDATES_ONLY, VERBOSE ] = read_config_file()
 
 
-    logfile.write( get_time() + ' Exited normally.' + '\n' )
+    if len( argv ) > 1:
+
+        for argument in argv[ 1 : ]:
+
+            if argument == '-s' or argument == '--silent':
+                SILENT = True
+            elif argument == '-v' or argument == '--verbose':
+                VERBOSE == True
+            elif argument == '-u' or argument == '--update-only':
+                UPDATES_ONLY = True
+            else:
+                logfile.write( get_time() + ' Illegal argument \'' + argument + '\' detected' + '\n' )
+                continue
+
+            logfile.write( get_time() + ' Program called with \'' + argument + '\'' + '\n' )
+
+
+
+
+
+
+
+
+    logfile.write( get_time() + ' Exited normally' + '\n' )
     logfile.close()
 
