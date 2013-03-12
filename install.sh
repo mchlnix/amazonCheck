b117914ce355212d5c50476103d7baa157a4c157
Michael Nix <mchl.nix@googlemail.com>
2013-03-12 20:19:03 +0100

    updates the install.sh script

--- install.sh
+++ install.sh
@@ -3,13 +3,21 @@
 #Gets the directory the files are in ( should be the same directory this script is in )
 basedir=$(pwd)/$(dirname $0)
 
+#Creates necessary directories and files in the home directory
+mkdir -p ~/.amazonCheck
+mkdir -p ~/.amazonCheck/pics
+touch ~/.amazonCheck/aC.log
+touch ~/.amazonCheck/aC.data
+touch ~/.amazonCheck/aC.config
+cp -v $basedir/icon.png ~/.amazonCheck/aC.png
+
 #Copies the application to its specific destination
 sudo cp -v $basedir/amazonCheck.py /usr/bin/amazonCheck
 
 #Copies the .desktop file to its specific destination
 sudo cp -v $basedir/amazonCheck.desktop /usr/share/applications/
 
-#Copies the icon file to its specific destination
+#Copies the icon file to its specific destinations
 sudo cp -v $basedir/icon.png /usr/share/pixmaps/amazonCheck.png
 
 #Installs necessary libraries ( python should be installed )
